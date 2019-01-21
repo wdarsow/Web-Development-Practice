d212612fa648da77ecf4f4f4e73235488f47aebf
Wade <wade.darsow@gmail.com>
2019-01-20 21:03:07 -0600

    basic object exercise

---
+++ Objects/objects2.js
@@ -0,0 +1,29 @@
+let posts = [
+    {
+        title: "cats",
+        author: "WD",
+        comments: ["comment1", "comment2"]
+    },
+    {
+        title: "dogs",
+        author: "CJ",
+        comments: ["dog comment1", "dog comment2"]
+    }
+]
+
+// console.log(posts[0].comments[1])
+
+// posts._name = 6;
+// console.log(posts);
+
+let someObject = {
+    friends:  [
+        {name: "Eddie"},
+        {name: "Julie"},
+        {name: "Bill"}
+    ], 
+    color: "baby blue",
+    isKind: true
+};
+
+console.log(someObject.friends[0].name);
